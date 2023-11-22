c265ea14f6d750c30ec1858c50915fbe7f0eb70a
José David <jotatrisol14@gmail.com>
2023-11-22 15:29:21 -0600

    Agregue el archivo del proyecto de NETBEANS

---
+++ ServiciosSociales/src/serviciossociales/ServiciosSociales.java
@@ -0,0 +1,17 @@
+
+package serviciossociales;
+
+/**
+ *
+ * @author Estudiante17
+ */
+public class ServiciosSociales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+    
+}
